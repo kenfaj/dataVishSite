dbece65e4c7e3b4470875be70f1eee226c462486
kenfaj <149665960+kenfaj@users.noreply.github.com>
2024-09-18 22:17:05 +0800

    initial with sample script js

---
+++ sample.js
@@ -0,0 +1,45 @@
+/*
+console.log('aaa');
+console.error('asdasd');
+console.warn('warningggg');
+*/
+/*use: let, const/ var is not recommended because it is a global variable*/
+/*dataytpes: String, Numbers, Boolean, null, undefined, Symbol*/
+
+const bobo = 'lyndon';
+const age = 99;
+const iq = 0.01;
+const isbobo= true;
+const x = null;
+const  y = undefined;
+let z;//also undefined
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof iq);
+console.log(typeof isbobo);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+//concatenation, old fashion
+console.log("bobo si "+bobo);
+console.log('bobo si ${bobo}');
+console.log(`bobo si ${bobo}`);
+
+//string manipulation
+//same java function names
+//split function is cool, makes an array when split string
+const s = `tech, comp, it`;
+
+console.log(s.split(`, `));
+
+//arrays
+const numbers = new Array(1,2,3,3,4,5);
+const fruits = ['apples', 'oranges', 'pears'];
+console.log(numbers);
+console.log(fruits);
+
+
+
+
+
